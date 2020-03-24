71823caf9df057af37f282ae89470b51828a6301
shaun <shauncarr2010@hotmail.com>
2020-03-24 11:43:17 -0700

    server update

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const BodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+
+const port = 5000;
+
 
 app.use(express.static('dist'));
+app.use(cors());
+app.use(BodyParser.json());
+
+
+app.get('/getStory', (req,res) => {
+    const boss = ['The Slave Lords', 'Zargon', 'Strahd von Zarovich', 'Verminaard']
+    
+    const motive = ['To prove that Zadera is dead', 'To kill all the humans', 'to bring fourth the darkness', 'the dark father demands it']
+    
+    const world = ['post-apotyltic', 'steampunk', 'dark-ages', 'golden-age']
+
+    const story = [boss[Math.floor(Math.random() * boss.length)], motive[Math.floor(Math.random() * motive.length)], world[Math.floor(Math.random() * world.length)]]
+
+    res.send(story)
+
+});
 
 app.listen(port, () => console.log(`listening from port: ${port}`));
--- src/components/Gen.jsx
+++ src/components/Gen.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import Axios from 'axios';
 
 class Gen extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            loading: true,
+            story: null
         };
     };
 
     GetStory() {
-        console.log('Button works')
+        Axios.get(`http://localhost:5000/getStory`)
+        .then((data) => {
+            this.setState({
+                loading: false,
+                story: data.data
+            })
+            console.log(this.state.story)
+        });
     };
 
     render() {
+        let p 
+        if(this.state.loading === false){
+            p = <p className="Gen__Story">This is working</p>
+        }
         return (
             <div>
                 <div>
@@ -22,6 +35,9 @@ class Gen extends React.Component {
                 <br/>
                 <div>
                     <p className="Gen__P">Generate a new story for your players. Just click the button below!</p>
+                </div>
+                <div>
+                    {p}
                 </div>
                 <br/>
                 <div className="Gen__Div">
